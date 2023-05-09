97a646166f2ed6fff39088ffa9c5606d7aed24bf
Raveena Nair <raveenanair@microsoft.com>
2023-05-09 16:23:05 -0700

    trying to fix

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+
+    return (
+        <main>
+            <h1>Hey Bestie!</h1>
+        </main>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/* Register all pages here. This has nothing to do with navigation bar, just the existence of the page. */
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
