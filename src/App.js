64ff4c0d1ff4da4c09f3e0c97349f84412ac02c2
Raveena Nair <raveenanair@microsoft.com>
2023-05-09 20:57:55 -0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from './pages/Home.js';
 import Profile from './pages/Profile.js';
+import Navbar from './components/Navbar.js';
 function App() {
   return (
     <Router>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
       <Route exact path='/' element={<Home/>}/>     
       <Route exact path='/profile' element={<Profile/>}/>
